073067cba71fbe90a7b5adc645f3146ed6d3b2c5
NayaPetu <hope_peturova@mail.ru>
2025-03-26 14:31:16 +0300

    л2 ничья

--- laba_2/Tic_Tac_Toe/src/App.tsx
+++ laba_2/Tic_Tac_Toe/src/App.tsx
@@ -18,23 +18,46 @@ function Board({ xIsNext, squares, onPlay }) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+  
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? "X" : "O";
+  
     onPlay(nextSquares);
   }
-
+  
   const winner = calculateWinner(squares);
-  let status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? "X" : "O"}`;
+  const isDraw = !winner && squares.every((square) => square !== null);
+  
+  let status;
+  if (winner) {
+    status = "Победил: " + winner;
+  } else if (isDraw) {
+    status = "Ничья";
+  } else {
+    status = "Ходит игрок: " + (xIsNext ? "X" : "O");
+  }
+  
+  
 
   return (
-    <div className="text-center">
-      <div className="text-xl font-semibold mb-4">{status}</div>
-      <div className="grid grid-cols-3 gap-1">
-        {squares.map((square, i) => (
-          <Square key={i} value={square} onSquareClick={() => handleClick(i)} />
-        ))}
+    <>
+      <div className="text-gray-500 dark:text-gray-500 p-4">{status}</div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 ">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-    </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
   );
 }
 
@@ -53,6 +76,20 @@ export default function Game() {
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Перейти на ход №' + move;
+    } else {
+      description = 'Начать игру заново';
+    }
+    return (
+      <li key={move}>
+        <button className="bg-gray-900 hover:bg-violet-600 focus:outline-1 focus:outline-offset-1 focus:outline-violet-500 active:bg-violet-700 mx-auto flex max-w-sm items-center gap-x-4 rounded-xl p-2 text-white" onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="flex flex-col items-center p-6">
@@ -96,3 +133,6 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
+
+
